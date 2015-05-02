5d3119d8c99d80c2929b84c0db90dd234e9fce4f
KLE-PA <klepasu@hotmail.com>
2015-05-02 05:16:30 -0500

    agregar menu principal

--- calculator.c
+++ calculator.c
@@ -1,6 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-main()
+int main()
 {
+int opcion;
+float a, b;
+	printf("Ingrese un numero: ");
+	scanf("%f", &a);
+	printf("Ingrese otro numero: ");
+	scanf("%f", &b);
 	
+do{
+	printf("1.SUMA");
+	printf("2.RESTA");
+	printf("3.MULTIPLICACION");			
+	printf("4.DIVISION");
+	printf("5.POTENCIA");
+	printf("6.RAIZ CUADRADA");
+	printf("7.RAIZ CUBICA");
+	printf("8.SENO");
+	printf("9.COSENO");
+	printf("10.TANGENTE");
+	printf("11.FACTORIAL");
+	printf("Ingrese una opcion: ");
+	scanf("%d", opcion");
+switch(opcion){
+	case 1:
+	break;
+	case 2:
+	break;
+	case 3:
+	break;
+	case 4:
+	break;
+	case 5:
+	break;
+	case 6:
+	break;
+	case 7:
+	break;
+	case 8:
+	break;
+	case 9:
+	break;
+	case 10:
+	break;
+	case 11:
+	break;
+}while (opcion!=0);
+
+return 0;
 }
