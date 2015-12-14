633d919a34307697ddf7b85c188d7702ec542a2c
Jondy-66 <jondy-66@hotmail.com>
2015-12-13 23:26:16 -0500

    Modificando archivo de division a archivo de cabecera

---
+++ division.h
@@ -0,0 +1,12 @@
+float division(int dividendo, int divisor)
+{
+    if (divisor) 
+ {
+    return dividendo / divisor;
+ }
+     else
+ {
+    printf ("No se puede dividir por cero\n");
+     return 0;
+ }
+}
