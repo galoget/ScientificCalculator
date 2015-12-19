4c65f7a2a50490fca38ab932d0b70d921f3f655d
Steven <steven-s-47@hotmail.com>
2015-12-18 23:02:25 -0500

    cambio en raiz cubica

---
+++ raizcubica.h
@@ -0,0 +1,12 @@
+#include <studio.h>
+#include <math.h>
+
+float raiz_cubica(float a){
+float b;
+//printf("Calculo de raiz cubica\n");
+//printf("Ingrese el numero...");
+//scanf("%f",&a);
+b=pow(a,1/3);
+return b
+//printf("\n La raiz cubica es:\t%f",b);
+}
